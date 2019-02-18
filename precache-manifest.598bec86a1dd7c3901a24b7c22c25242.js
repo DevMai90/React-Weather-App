2553a228ead76741dff761dbe98231d9c4b91bec
DevMai90 <dmai90@yahoo.com>
2019-02-18 13:10:53 -0800

    Updates

---
+++ precache-manifest.598bec86a1dd7c3901a24b7c22c25242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4cfa3beddf0241a8a8",
+    "url": "/react-weather-app/static/js/runtime~main.4c4cfa3b.js"
+  },
+  {
+    "revision": "8381a09a9445defaa9b4",
+    "url": "/react-weather-app/static/js/main.8381a09a.chunk.js"
+  },
+  {
+    "revision": "e1f24afe4f5befe19bf1",
+    "url": "/react-weather-app/static/js/2.e1f24afe.chunk.js"
+  },
+  {
+    "revision": "8381a09a9445defaa9b4",
+    "url": "/react-weather-app/static/css/main.75444a71.chunk.css"
+  },
+  {
+    "revision": "9bf8528483b250d9d57800bda7d026fc",
+    "url": "/react-weather-app/index.html"
+  }
+];
