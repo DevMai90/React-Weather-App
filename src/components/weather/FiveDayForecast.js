25cd92e6bd0b0f82e7e3ae37fc65eab0f5a330dc
DevMai90 <dmai90@yahoo.com>
2019-02-21 23:34:29 -0800

    Minor updates. Made notes to continue

--- src/components/weather/FiveDayForecast.js
+++ src/components/weather/FiveDayForecast.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
 
 class FiveDayForecast extends Component {
   render() {
     return (
-      <div>
-        <h1>wesdgh</h1>
-        <p>asrdfgh</p>
-      </div>
+      <Consumer>
+        {value => {
+          const { list } = value;
+          const daily = list.map(day => {
+            return {
+              condition: day.weather[0].description,
+              temperature: (((day.main.temp - 273.15) * 9) / 5 + 32).toFixed(0),
+              humidity: day.main.humidity,
+              wind: day.wind.speed,
+              icon: day.weather[0].icon
+            };
+          });
+
+          // COLLECT THE DAILY TEMP AND PUT INTO A SEPARATE ARRAY. TRY ARRAY POSITION OR MATCH THE TIMES
+          const fiveDayForecast = [];
+
+          return (
+            <div className="card mt-5 mb-3">
+              <div className="card-body bg-primary text-white text-center">
+                <h3>Five Day Forecast</h3>
+                <p>{console.log(daily)}</p>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
