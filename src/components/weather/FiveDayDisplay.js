18a387457056d74a3fac8387f2771476730c50ed
DevMai90 <dmai90@yahoo.com>
2019-03-08 22:37:06 -0800

    Added weather cards for 5 day forecast

--- src/components/weather/FiveDayDisplay.js
+++ src/components/weather/FiveDayDisplay.js
@@ -3,23 +3,22 @@ import React from 'react';
 const FiveDayDisplay = props => {
   const { forecast } = props;
 
-  const week = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
+  const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   return (
     <div className="d-flex justify-content-center row-hl">
       <div className="item-hl">
         <div className="card">
           <div className="card-body">
-            <div className="card-title">
+            <div className="card-title text-center">
               {week[forecast[0].dayOne.date.getDay()]}
             </div>
+            <img
+              src={`http://openweathermap.org/img/w/${
+                forecast[0].dayOne.icon
+              }.png`}
+              alt=""
+            />
+            <h4>{forecast[0].dayOne.temperature}° F</h4>
           </div>
         </div>
       </div>
@@ -27,9 +26,16 @@ const FiveDayDisplay = props => {
       <div className="item-hl">
         <div className="card">
           <div className="card-body">
-            <div className="card-title">
+            <div className="card-title text-center">
               {week[forecast[1].dayTwo.date.getDay()]}
             </div>
+            <img
+              src={`http://openweathermap.org/img/w/${
+                forecast[1].dayTwo.icon
+              }.png`}
+              alt=""
+            />
+            <h4>{forecast[1].dayTwo.temperature}° F</h4>
           </div>
         </div>
       </div>
@@ -37,9 +43,16 @@ const FiveDayDisplay = props => {
       <div className="item-hl">
         <div className="card">
           <div className="card-body">
-            <div className="card-title">
+            <div className="card-title text-center">
               {week[forecast[2].dayThree.date.getDay()]}
             </div>
+            <img
+              src={`http://openweathermap.org/img/w/${
+                forecast[2].dayThree.icon
+              }.png`}
+              alt=""
+            />
+            <h4>{forecast[2].dayThree.temperature}° F</h4>
           </div>
         </div>
       </div>
@@ -47,9 +60,16 @@ const FiveDayDisplay = props => {
       <div className="item-hl">
         <div className="card">
           <div className="card-body">
-            <div className="card-title">
+            <div className="card-title text-center">
               {week[forecast[3].dayFour.date.getDay()]}
             </div>
+            <img
+              src={`http://openweathermap.org/img/w/${
+                forecast[3].dayFour.icon
+              }.png`}
+              alt=""
+            />
+            <h4>{forecast[3].dayFour.temperature}° F</h4>
           </div>
         </div>
       </div>
@@ -57,9 +77,16 @@ const FiveDayDisplay = props => {
       <div className="item-hl">
         <div className="card">
           <div className="card-body">
-            <div className="card-title">
+            <div className="card-title text-center">
               {week[forecast[4].dayFive.date.getDay()]}
             </div>
+            <img
+              src={`http://openweathermap.org/img/w/${
+                forecast[4].dayFive.icon
+              }.png`}
+              alt=""
+            />
+            <h4>{forecast[4].dayFive.temperature}° F</h4>
           </div>
         </div>
       </div>
