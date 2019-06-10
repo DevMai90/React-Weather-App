80ada9535dec3fe2b1c5c7307511dab77d16f095
DevMai90 <dmai90@yahoo.com>
2019-06-10 16:20:34 -0700

    Updates

---
+++ precache-manifest.59b92d7214038f0cd748ed11823fc358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdf5b9952d284215c994",
+    "url": "/React-Weather-App/static/css/main.a3392078.chunk.css"
+  },
+  {
+    "revision": "bdf5b9952d284215c994",
+    "url": "/React-Weather-App/static/js/main.bdf5b995.chunk.js"
+  },
+  {
+    "revision": "73649db39fa5d86660bf",
+    "url": "/React-Weather-App/static/js/runtime~main.73649db3.js"
+  },
+  {
+    "revision": "5f6f7518cd6073604dfc",
+    "url": "/React-Weather-App/static/js/2.5f6f7518.chunk.js"
+  },
+  {
+    "revision": "4b8ada29d061840ece0e01d6756bd6af",
+    "url": "/React-Weather-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Weather-App/precache-manifest.3847c42020c63da0d757429cd1ef9dd8.js"
+  "/React-Weather-App/precache-manifest.59b92d7214038f0cd748ed11823fc358.js"
 );
 
 workbox.clientsClaim();
